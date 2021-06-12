75cbec7be06730b5b212f10b69cdbd3d918337d2
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-06-12 12:38:11 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const FORMAT_SYNTAX: string[] = [
     "{{DATE}}", "{{DATE:<DATEFORMAT>}}", "{{VDATE:<VARIABLE NAME>, <DATE FORMAT>}}",
-    "{{VALUE}}", "{{NAME}}", "{{VALUE:<VARIABLE NAME>}}", "{{LINKCURRENT}}"
+    "{{VALUE}}", "{{NAME}}", "{{VALUE:<VARIABLE NAME>}}", "{{LINKCURRENT}}", "{{MACRO:<MACRONAME>}}"
 ];
 
 export const FILE_NAME_FORMAT_SYNTAX: string[] = [
