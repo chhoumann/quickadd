62c67fee458c883e7d880be6168bcc41657880d4
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-07-03 15:10:34 +0200

    Remove unnecessary trycatch to fix #45

--- src/engine/TemplateChoiceEngine.ts
+++ src/engine/TemplateChoiceEngine.ts
@@ -16,52 +16,47 @@ export class TemplateChoiceEngine extends TemplateEngine {
     }
 
     public async run(): Promise<void> {
-        try {
-            let folderPath: string = "";
+        let folderPath: string = "";
 
-            if (this.choice.folder.enabled) {
-                let folders: string[] = this.choice.folder.folders;
+        if (this.choice.folder.enabled) {
+            let folders: string[] = this.choice.folder.folders;
 
-                if (this.choice.folder?.chooseWhenCreatingNote) {
-                    folders = await getAllFolders(this.app);
-                }
-
-                folderPath = await this.getOrCreateFolder(folders);
+            if (this.choice.folder?.chooseWhenCreatingNote) {
+                folders = await getAllFolders(this.app);
             }
 
-            let filePath;
-
-            if (this.choice.fileNameFormat.enabled) {
-                filePath = await this.getFormattedFilePath(folderPath, this.choice.fileNameFormat.format, this.choice.name);
-            } else {
-                filePath = await this.getFormattedFilePath(folderPath, VALUE_SYNTAX, this.choice.name);
-            }
-
-            if (this.choice.incrementFileName)
-                filePath = await this.incrementFileName(filePath);
-
-            const createdFile: TFile = await this.createFileWithTemplate(filePath, this.choice.templatePath);
-            if (!createdFile) {
-                log.logWarning(`Could not create file '${filePath}'.`);
-                return;
-            }
-
-            if (this.choice.appendLink) {
-                appendToCurrentLine(this.app.fileManager.generateMarkdownLink(createdFile, ''), this.app);
-            }
-
-            if (this.choice.openFile) {
-                if (!this.choice.openFileInNewTab.enabled) {
-                    await this.app.workspace.activeLeaf.openFile(createdFile);
-                } else {
-                    await this.app.workspace
-                        .splitActiveLeaf(this.choice.openFileInNewTab.direction)
-                        .openFile(createdFile);
-                }
-            }
+            folderPath = await this.getOrCreateFolder(folders);
         }
-        catch (e) {
-            log.logError(e.message);
+
+        let filePath;
+
+        if (this.choice.fileNameFormat.enabled) {
+            filePath = await this.getFormattedFilePath(folderPath, this.choice.fileNameFormat.format, this.choice.name);
+        } else {
+            filePath = await this.getFormattedFilePath(folderPath, VALUE_SYNTAX, this.choice.name);
+        }
+
+        if (this.choice.incrementFileName)
+            filePath = await this.incrementFileName(filePath);
+
+        const createdFile: TFile = await this.createFileWithTemplate(filePath, this.choice.templatePath);
+        if (!createdFile) {
+            log.logWarning(`Could not create file '${filePath}'.`);
+            return;
+        }
+
+        if (this.choice.appendLink) {
+            appendToCurrentLine(this.app.fileManager.generateMarkdownLink(createdFile, ''), this.app);
+        }
+
+        if (this.choice.openFile) {
+            if (!this.choice.openFileInNewTab.enabled) {
+                await this.app.workspace.activeLeaf.openFile(createdFile);
+            } else {
+                await this.app.workspace
+                    .splitActiveLeaf(this.choice.openFileInNewTab.direction)
+                    .openFile(createdFile);
+            }
         }
     }
 }
