33c9e8208a14122f9f589ba580cc70acf024b10d
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-02-22 13:35:46 +0100

    chore: use vitest instead of jest

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vitest/config";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import * as path from "path";
+
+export default defineConfig({
+	plugins: [
+		svelte({
+			hot: !process.env.VITEST,
+			preprocess: sveltePreprocess(),
+		}),
+	],
+	resolve: {
+		alias: {
+			src: path.resolve("./src"),
+		},
+	},
+	test: {
+		include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		globals: true,
+		environment: "jsdom",
+	},
+});
