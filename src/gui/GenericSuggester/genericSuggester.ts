1032bbf181242af4124039e9891005fb78802180
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-03-07 14:09:44 +0100

    safer access to undocumented chooser

--- src/gui/GenericSuggester/genericSuggester.ts
+++ src/gui/GenericSuggester/genericSuggester.ts
@@ -24,21 +24,24 @@ export default class GenericSuggester extends FuzzySuggestModal<string> {
 			this.rejectPromise = reject;
 		});
 
-        this.inputEl.addEventListener('keydown', (event :KeyboardEvent) => {
-            if (event.code == 'Tab') {
-                let complete = ""
-                // @ts-ignore
-                const divNode = this.chooser.suggestions[this.chooser.selectedItem]
-                for (const node of divNode.childNodes) {
-                    if (node.nodeName == '#text') {
-                        complete += node.textContent
-                    } else if (node.nodeName == 'SPAN') {
-                        complete += node.innerText
-                    }
-                }
-                this.inputEl.value = complete
-            }
-        })
+		this.inputEl.addEventListener("keydown", (event: KeyboardEvent) => {
+			// chooser is undocumented & not officially a part of the Obsidian API, hence the precautions in using it.
+			if (event.code !== "Tab" || !("chooser" in this)) {
+				return;
+			}
+
+			const { values, selectedItem } = this.chooser as {
+				values: {
+					item: string;
+					match: { score: number; matches: unknown[]; };
+				}[];
+				selectedItem: number;
+				[key: string]: unknown;
+			};
+
+			const { value } = this.inputEl;
+			this.inputEl.value = values[selectedItem].item ?? value;
+		});
 
 		this.open();
 	}
